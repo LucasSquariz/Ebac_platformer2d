222ebdfddd977f9f429a1e6ee53039747205f8b1
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2023-05-16 17:45:48 -0300

    :sparkles: Feat: run mode

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidBody2D;
     public Vector2 friction = new Vector2(-.1f,0);
     public float speed;
+    public float speedRun;
     public float jumpForce = 25;
+
+    private float _currentSpeed;    
     void Update()
     {
         HandleJump();
@@ -16,15 +19,24 @@ public class Player : MonoBehaviour
 
     public void HandleMovement()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _currentSpeed = speedRun;
+        }
+        else
+        {
+            _currentSpeed = speed;
+        }        
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody2D.MovePosition(myRigidBody2D.position - velocity * Time.deltaTime);
-            myRigidBody2D.velocity = new Vector2(-speed, myRigidBody2D.velocity.y);
+            myRigidBody2D.velocity = new Vector2(-_currentSpeed, myRigidBody2D.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             // myRigidBody2D.MovePosition(myRigidBody2D.position + velocity * Time.deltaTime);
-            myRigidBody2D.velocity = new Vector2(speed, myRigidBody2D.velocity.y);
+            myRigidBody2D.velocity = new Vector2(_currentSpeed, myRigidBody2D.velocity.y);
         }
 
         if(myRigidBody2D.velocity.x > 0)
